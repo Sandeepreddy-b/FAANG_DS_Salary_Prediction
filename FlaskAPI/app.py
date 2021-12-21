a5808f1665b1d205b4944c09a4c09f0c655d254e
SANDEEP REDDY BUTUKURI <bsreddy1211@gmail.com>
2021-12-21 06:07:11 +0530

    flask api

---
+++ FlaskAPI/app.py
@@ -0,0 +1,94 @@
+import flask
+from flask import Flask, jsonify, request
+import json
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+
+def load_models():
+    file_name = "models/model_file.p"
+    with open(file_name, 'rb') as pickled:
+        data = pickle.load(pickled)
+        model = data['model']
+    return model
+
+@app.route('/predict', methods=['GET'])
+
+
+
+def predict():
+    # stub input features 
+    x = np.array([0.0,
+         2020.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         1.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         1.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0,
+         0.0]).reshape(1,-1)
+    # load model
+    model = load_models()
+    prediction = model.predict(x)[0]
+    response = json.dumps({'response': prediction})
+    return response, 200
+
+if __name__ == '__main__':
+    application.run(debug=True)
